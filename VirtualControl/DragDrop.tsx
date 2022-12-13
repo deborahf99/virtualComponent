5e94cc424ba146ccdc7e5e71edd3501e1e6ddf12
IT20026448 <it20026448@my.sliit.lk>
2022-12-13 19:35:57 +0530

    debicommit

--- VirtualControl/DragDrop.tsx
+++ VirtualControl/DragDrop.tsx
@@ -51,9 +51,10 @@ export default function DragDropFile() {
             <label id="label-file-upload" htmlFor="input-file-upload" className={dragActive ? "drag-active" : "" }>
             <div>
             <svg xmlns="http://www.w3.org/2000/svg" id="mdi-file-document" viewBox="-20 0 60 24"><path d="M13,9H18.5L13,3.5V9M6,2H14L20,8V20A2,2 0 0,1 18,22H6C4.89,22 4,21.1 4,20V4C4,2.89 4.89,2 6,2M15,18V16H6V18H15M18,14V12H6V14H18Z" /></svg>
-                <p>
+                <p style={{color: "darkgrey"}}>
                     <a className="upload-button" onClick={onButtonClick}>Click to upload</a>or drag and drop
                 </p>
+                <p style={{fontSize: "15px", color: "darkgrey"}}>Maximum file size 50MB</p>
             </div> 
             </label>
         { dragActive && <div id="drag-file-element" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}></div> }

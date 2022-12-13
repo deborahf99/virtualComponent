fa8967fbf04740beb5905ffd2533d7309104f926
IT20026448 <it20026448@my.sliit.lk>
2022-12-14 02:02:09 +0530

    debicommit

--- VirtualControl/DragDrop.tsx
+++ VirtualControl/DragDrop.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import './App.css';
+import {NeutralColors} from "@fluentui/theme";
+import { ChangeEvent, useState } from 'react';
 
 export default function DragDropFile() {
     // drag state
   const [dragActive, setDragActive] = React.useState(false);
-  const [files, setFiles] = React.useState({});
+  const [fileList, setFiles] = useState<FileList | null>(null);
   // ref
   const inputRef = React.useRef(null);
   
@@ -30,13 +32,14 @@ export default function DragDropFile() {
   };
   
   // triggers when file is selected with click
-  const handleChange = function(e : any) {
+  const handleChange = function(e: ChangeEvent<HTMLInputElement>) {
     e.preventDefault();
     if (e.target.files && e.target.files[0]) {
-      // handleFiles(e.target.files);
+        setFiles(e.target.files);
     }
   };
   
+  const files = fileList ? [...fileList] : [];
 // triggers the input when the button is clicked.
   const onButtonClick = () => {
     inputRef.current.click();
@@ -45,20 +48,33 @@ export default function DragDropFile() {
   return (
     <div>
         <form id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
-            <h3>Upload and attach files</h3>
-            <h4 style={{color: "darkgrey"}}>Upload and attach files to this project.</h4>
+            <div style={{textAlign: "left"}}>
+                <h3>Upload and attach files</h3>
+                <h4 style={{color: NeutralColors.gray100}}>Upload and attach files to this project.</h4>
+            </div>
             <input ref={inputRef} type="file" id="input-file-upload" multiple={true} onChange={handleChange} />
             <label id="label-file-upload" htmlFor="input-file-upload" className={dragActive ? "drag-active" : "" }>
             <div>
             <svg xmlns="http://www.w3.org/2000/svg" id="mdi-file-document" viewBox="-20 0 60 24"><path d="M13,9H18.5L13,3.5V9M6,2H14L20,8V20A2,2 0 0,1 18,22H6C4.89,22 4,21.1 4,20V4C4,2.89 4.89,2 6,2M15,18V16H6V18H15M18,14V12H6V14H18Z" /></svg>
-                <p style={{color: "darkgrey"}}>
+                <p style={{color: NeutralColors.gray120}}>
                     <a className="upload-button" onClick={onButtonClick}>Click to upload</a>or drag and drop
                 </p>
-                <p style={{fontSize: "15px", color: "darkgrey"}}>Maximum file size 50MB</p>
+                <p style={{fontSize: "15px", color: NeutralColors.gray120}}>Maximum file size 50MB</p>
             </div> 
             </label>
-        { dragActive && <div id="drag-file-element" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}></div> }
+            { dragActive && <div id="drag-file-element" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}></div> }
+            <button style={{width: "35%", height: "15%", marginRight: "10px", backgroundColor: "white", borderRadius: "6px"}}>Cancel</button>
+            <button style={{width: "35%", height: "15%", backgroundColor: "black", color:"white", borderRadius: "6px"}}>Attach files</button>
         </form>
+        <div style={{position: "fixed", marginTop: "30%"}}>
+            <ul>
+                {files.map((file, i) => (
+                    <li key={i}>
+                        {file.name} - {file.type}
+                    </li>
+                ))}
+            </ul>
+        </div>
     </div>
   );
 }
